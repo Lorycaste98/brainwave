26484eec1e9b8a8db39b906b51da538b4c39e4a0
Lorenzo <lorenzo.castelletti98@gmail.com>
2024-11-12 11:24:00 +0100

    completato componente Header

--- src/components/design/Header.jsx
+++ src/components/design/Header.jsx
@@ -28,17 +28,11 @@ export const BackgroundCircles = () => {
   );
 };
 
-export const HambugerMenu = () => {
+export const HamburgerMenu = () => {
   return (
     <div className="absolute inset-0 pointer-events-none lg:hidden">
       <div className="absolute inset-0 opacity-[.03]">
-        <img
-          className="w-full h-full object-cover"
-          src={background}
-          width={688}
-          height={953}
-          alt="Background"
-        />
+        <img className="w-full h-full object-cover" src={background} width={688} height={953} alt="Background" />
       </div>
 
       <Rings />
